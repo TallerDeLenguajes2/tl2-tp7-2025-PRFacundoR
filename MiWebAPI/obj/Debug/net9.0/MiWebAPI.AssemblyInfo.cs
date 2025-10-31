cdc00b9d07b4d5b9faa9367a162af460a67cae93
PeraltaRomeroFacundoRodolfo <facundoperalta217@gmail.com>
2025-10-31 13:26:51 -0300

    falta los endpoints

--- MiWebAPI/obj/Debug/net9.0/MiWebAPI.AssemblyInfo.cs
+++ MiWebAPI/obj/Debug/net9.0/MiWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05adef20524edc81d9166946aea1c57f75be2a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4db4a1a7606f560383b727a8ae8581e5872f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
